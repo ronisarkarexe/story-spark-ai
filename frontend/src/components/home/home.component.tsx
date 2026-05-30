01ea041ce4f7ea0183235af3d595e1df07668a57
toufiq00007 <toufiqsuri2006@gmail.com>
2026-05-30 12:32:58 +0530

    Change in frontend

--- frontend/src/components/home/home.component.tsx
+++ frontend/src/components/home/home.component.tsx
@@ -15,7 +15,7 @@ const HomeComponent = () => {
   const isLogin = isLoggedIn();
   return (
     <>
-      <div className="story-page-shell grid grid-cols-12 items-start gap-6 py-12 sm:gap-8 lg:gap-10 lg:py-16">
+      <div className="story-page-shell grid grid-cols-12 items-start gap-8 lg:gap-16 py-12 lg:py-16">
         <div className="col-span-12 lg:col-span-8 min-w-0">
           <FeatureComponent />
           <LatestPostsComponent />
