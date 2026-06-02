5bfb0371fb3be18644279816698b5446e5c3f939
Krishna Kumar <krishnakumarsharma8077@gmail.com>
2026-06-02 16:03:29 +0530

    Fix axios interceptor registration and rate limit store issues

--- frontend/src/helpers/axios/axionInstance.ts
+++ frontend/src/helpers/axios/axionInstance.ts
@@ -1,68 +1,87 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
 import { getFromLocalStorage } from "../../utils/local-storage";
 import { AUTH_KEY } from "../../constants/storage-key";
 import { IMeta, ResponseErrorType } from "../../types";
 
-const instance = axios.create();
+type AxiosGlobalState = typeof globalThis & {
+  __storySparkAxiosInstance?: AxiosInstance;
+  __storySparkAxiosInterceptorsRegistered?: boolean;
+};
+
+const axiosGlobalState = globalThis as AxiosGlobalState;
+
+const instance = axiosGlobalState.__storySparkAxiosInstance ?? axios.create();
+axiosGlobalState.__storySparkAxiosInstance = instance;
+
 instance.defaults.headers.post["Content-Type"] = "application/json";
 instance.defaults.headers["Accept"] = "application/json";
 instance.defaults.timeout = 60000;
 
-export interface ApiResponseData<T = unknown> {
+interface ApiResponseData<T = unknown> {
   data: T;
   meta?: IMeta | undefined;
 }
 
-instance.interceptors.request.use(
-  function (config) {
-    const accessToken = getFromLocalStorage(AUTH_KEY);
-    if (accessToken) {
-      config.headers.Authorization = accessToken;
-    }
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
+export const setupAxiosInterceptors = () => {
+  if (axiosGlobalState.__storySparkAxiosInterceptorsRegistered) {
+    return;
   }
-);
 
-instance.interceptors.response.use(
-  (response: AxiosResponse<ApiResponseData>) => {
-    return response;
-  },
-  function (error) {
-    let errorObject: ResponseErrorType;
-    if (error.code === "ERR_NETWORK") {
-      errorObject = {
-        statusCode: 503,
-        message: "Network Error - Unable to connect to the server",
-        errorMessages: [
-          {
-            path: "",
-            message: "Please check your internet connection and try again",
-          },
-        ],
-      };
-    } else if (error.response) {
-      errorObject = {
-        statusCode: error.response.data?.statusCode || 500,
-        message: error.response.data?.message || "Something went wrong!",
-        errorMessages: error.response.data?.errorMessage || [],
-      };
-    } else {
-      errorObject = {
-        statusCode: 500,
-        message: error.message || "Something went wrong!",
-        errorMessages: [
-          {
-            path: "",
-            message: "An unexpected error occurred",
-          },
-        ],
-      };
+  instance.interceptors.request.use(
+    function (config) {
+      const accessToken = getFromLocalStorage(AUTH_KEY);
+      if (accessToken) {
+        config.headers.Authorization = accessToken;
+      }
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
     }
-    return Promise.reject(errorObject);
-  }
-);
+  );
+
+  instance.interceptors.response.use(
+    (response: AxiosResponse<ApiResponseData>) => {
+      return response;
+    },
+    function (error) {
+      let errorObject: ResponseErrorType;
+      if (error.code === "ERR_NETWORK") {
+        errorObject = {
+          statusCode: 503,
+          message: "Network Error - Unable to connect to the server",
+          errorMessages: [
+            {
+              path: "",
+              message: "Please check your internet connection and try again",
+            },
+          ],
+        };
+      } else if (error.response) {
+        errorObject = {
+          statusCode: error.response.data?.statusCode || 500,
+          message: error.response.data?.message || "Something went wrong!",
+          errorMessages: error.response.data?.errorMessage || [],
+        };
+      } else {
+        errorObject = {
+          statusCode: 500,
+          message: error.message || "Something went wrong!",
+          errorMessages: [
+            {
+              path: "",
+              message: "An unexpected error occurred",
+            },
+          ],
+        };
+      }
+      return Promise.reject(errorObject);
+    }
+  );
+
+  axiosGlobalState.__storySparkAxiosInterceptorsRegistered = true;
+};
+
+setupAxiosInterceptors();
 
 export { instance };
