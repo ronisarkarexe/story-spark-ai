4e8d81564315dfd021b8aa17a8eafd26d3301b3e
kashviporwal-byte <kashviporwal-byte@users.noreply.github.com>
2026-06-12 23:47:04 +0530

    fix(backend): resolve frontend auth module imports and other syntax errors #2972

--- backend/src/app/modules/reaction/reaction.service.ts
+++ backend/src/app/modules/reaction/reaction.service.ts
@@ -21,9 +21,6 @@ const toggleReaction = async (
   }
 
   const post = await Post.findOne({
-  _id: postId,
-  isDeleted: { $ne: true },
-}).select("likesCount reactions");
     _id: postId,
     isDeleted: { $ne: true },
   }).select("likesCount reactions");
@@ -32,9 +29,6 @@ const toggleReaction = async (
     throw new ApiError(httpStatus.BAD_REQUEST, "Post not found!");
   }
 
-//  main
-    const newReaction = await Reaction.create({
-      postId: new Types.ObjectId(postId),
   const existingReaction = await Reaction.findOne({
     postId: post._id,
     userId: user._id,
@@ -71,6 +65,7 @@ const toggleReaction = async (
       userId: user._id,
       type: type,
     });
+
     post.reactions = post.reactions || [];
     post.reactions.push(newReaction._id);
     post.likesCount = (post.likesCount || 0) + 1;
